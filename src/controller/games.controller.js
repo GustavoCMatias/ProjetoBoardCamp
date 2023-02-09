efdedb2279a5568cf7444de24c3623be06c50bce
GustavoCMatias <gustavocmatias@gmail.com>
2023-02-09 09:29:59 -0300

    fix/Post games consertado e funcionando

--- src/controller/games.controller.js
+++ src/controller/games.controller.js
@@ -4,7 +4,7 @@ import { db } from "../database/database.connection.js"
 export async function inserirJogos(req, res){
     const {name, image, stockTotal, pricePerDay} = req.body
     try{
-        await db.query('INSERT INTO games (name, image, stockTotal, pricePerDay) VALUES ($1, $2, $3, $4)', [name, image, stockTotal, pricePerDay])
+        await db.query('INSERT INTO games (name,image,"stockTotal","pricePerDay") VALUES ($1, $2, $3, $4)', [name, image, stockTotal, pricePerDay])
         res.sendStatus(201)
     }catch(err){
         res.status(500).send(err.message)
